10156e66ab41f66666df7d64faf071aa717b8d6e
Vikas Mishra <vikasmishra99195@gmail.com>
2023-04-18 09:04:47 +0530

    String All Topic Code

--- lib/main.dart
+++ lib/main.dart
@@ -44,8 +44,57 @@ void main(){
 
   // var name1 = "Arjun";
   // var name2 = "Swapna";
-  // print(name1 +' '+ name2);
+  // // print(name1 +' '+ name2);
+  // print(name1 + name2);
 
+  // var msg = "Hey";
+  // var name = "Arjun";
+  // print(msg +' My name is '+ name);
+
+  // ##  Raw string
+
+  // var s = r'In a raw String, not even \n gets special treatment.';
+  // print(s);       ## r''  is used to print Raw String
+
+
+  // var s = r'In a raw String, not even '
+  //     r'\n gets special '
+  //     r'treatment.';
+  // print(s);
+
+  // Note : \n is used to change line
+
+  // var text = 'Hello My name is Vikas Mishra from Madhya Pradesh. \n i did my graduation in BCA and \n i am pursuing MCA from BU.';
+  // print(text);
+
+  // ## Some Important String properties and Methods
+
+  // var name = "Karn, Arjun";
+  // print(name.length);
+  // print(name.toLowerCase());
+  // print(name.toUpperCase());
+  // print(name.isEmpty);
+  // print(name.isNotEmpty);
+  // print(name.contains('a'));     true ## String contain a given word or not?
+  // print(name.contains('Ka'));       true
+  // print(name.contains('aK'));       false
+
+  // var name1 = "Arjun";
+  // print(name1.padLeft(10));
+  // print(name1.padRight(10));
+
+  // var name2 = "     Hello    ";
+  // print(name2.trim());
+  // print(name2.trimLeft());
+  // print(name2.trimRight());
+
+  // var name3 = "Arjun Karn";
+  // print(name3.split(' '));     split String on Space basic
+
+  // var name4 = 'Vikas&Akash';
+  // print(name4.split('&'));
+
+  // # String Topic Complete
 
 
 }
