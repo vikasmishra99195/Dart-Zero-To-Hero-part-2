f8234db6d93126ae7f33f76069dbdd470bb043f0
Vikas Mishra <vikasmishra99195@gmail.com>
2023-04-18 08:31:47 +0530

    Dart Zero To Hero Part-2

---
+++ android/app/src/main/java/com/example/string_practice_set/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.string_practice_set;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+void main(){
+
+  // # String
+
+  // String name1 = 'vikas';
+  // print(name1);
+  // print(name1.runtimeType);    .runtimetype is used to show the data type
+
+  // String name2 = "Vikas Mishra";
+  // print(name2);
+
+  // String name3 = "It's a language";
+  // print(name3);
+
+  // String name4 = 'My name is "Vikas Mishra" ';
+  // print(name4);
+
+  // ## for multi line String
+
+  // String name5 = ''' Hello My Name Is arjun
+  // and i'm from delhi india''';
+  // print(name5);
+
+  // String name6 = """ hello My name is arjun
+  // and i'm from delhi india""";
+  // print(name6);
+
+  // ## Expression Inside String
+
+  // var name = "arjun";
+  // print("$name");
+  // print('$name');
+  // print('${name}');
+  // print("${name}");
+  // print("My name is $name");
+  // print("My name is $name.toUpperCase()");     not work
+  // print("My name is ${name.toUpperCase()}");   brackets is compulsory when we use any function otherwise function not work
+
+  // ## String Concatenation
+
+  // print('Hello Arjun');
+  // print('Hello'+'Arjun');          ## we can concat two String by using + symbol
+  // print('hello' +' '+'arjun');
+
+  // var name1 = "Arjun";
+  // var name2 = "Swapna";
+  // print(name1 +' '+ name2);
+
+
+
+
+}
